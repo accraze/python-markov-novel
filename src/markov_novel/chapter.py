f6acc5b1c890559e756056955e5b29459f451712
accraze <accraze@gmail.com>
2016-11-26 15:18:09 -0800

    feat(chapter): added model to contain paragraphs

---
+++ src/markov_novel/chapter.py
@@ -0,0 +1,32 @@
+from random import randint
+
+import markovify
+from paragraph import Paragraph
+
+
+class Chapter(object):
+    """
+    A chapter
+    that contains paragraphs
+    written by markov models.
+    """
+
+    def __init__(self, model, title):
+        if not isinstance(model, markovify.Text):
+            raise Exception('model must be a markov model')
+        self.model = model
+        self.title = 'Chapter %d' % title
+
+    def write_chapter(self):
+        """
+        Create a chapter
+        that contains a random number
+        of paragraphs
+        """
+        self.paragraphs = []
+        self.paragraphs.append('\n')
+        for x in xrange(randint(0, 50)):
+            p = Paragraph(self.model)
+            self.paragraphs.append(p.get_paragraph())
+            self.paragraphs.append('\n')
+        return self.paragraphs
