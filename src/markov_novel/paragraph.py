485e448a65889ab2b8b5cf198c1c25a598dfd370
accraze <accraze@gmail.com>
2016-11-26 15:18:44 -0800

    feat(paragraph): added model to create lists of sentences

---
+++ src/markov_novel/paragraph.py
@@ -0,0 +1,33 @@
+from random import randint
+
+import markovify
+
+
+class Paragraph(object):
+    """
+    Object to hold
+    multiple setences created by
+    a Markov Model
+    """
+
+    def __init__(self, model):
+        if not isinstance(model, markovify.Text):
+            raise Exception('model must be a markov model')
+        self.model = model
+
+    def get_paragraph(self):
+        """
+        Write a paragraph
+        of 5 sentences.
+        """
+        self.text = ''
+        for x in xrange(randint(5, 12)):
+            sentence = self._write_sentence()
+            self.text = self.text + sentence
+        return self.text
+
+    def _write_sentence(self):
+        sentence = None
+        while sentence is None:
+            sentence = self.model.make_sentence()
+        return sentence + ' '
