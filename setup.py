73c645bcb1671a737ae272386eb741445754c2d0
accraze <accraze@gmail.com>
2016-11-26 15:16:59 -0800

    chore(build): sorted imports and pep8 cleanup

--- setup.py
+++ setup.py
@@ -65,7 +65,7 @@ setup(
         # eg: 'keyword1', 'keyword2', 'keyword3',
     ],
     install_requires=[
-        # eg: 'aspectlib==1.1.1', 'six>=1.7',
+        'markovify==0.4.3'
     ],
     extras_require={
         # eg:
