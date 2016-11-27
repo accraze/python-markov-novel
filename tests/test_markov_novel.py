9fbf8d6564513d2a117904db24f5b8685f98e67c
accraze <accraze@gmail.com>
2016-11-26 16:34:58 -0800

    test(markov-novel): added basic functional test

--- tests/test_markov_novel.py
+++ tests/test_markov_novel.py
@@ -1,6 +1,29 @@
+import os
+import shutil
+
+import markovify
 
 import markov_novel
 
 
 def test_main():
-    assert markov_novel  # use your library here
+    """
+    Basic functional test
+    """
+    assert markov_novel
+    path = 'tmp'
+    os.makedirs(path)
+    os.chdir(path)
+    # Get raw text as string.
+    from os.path import dirname, abspath
+    filename = os.path.join(
+        dirname(dirname(abspath(__file__))), 'tests/futuristmanifest.txt')
+    with open(filename) as f:
+        text = f.read()
+    # Build the model.
+    text_model = markovify.Text(text)
+    novel = markov_novel.Novel(text_model, chapter_count=1)
+    novel.write(novel_title='my-novel', filetype='md')
+    assert os.path.exists(os.path.join(os.getcwd(), 'my-novel.md'))
+    os.chdir(os.pardir)
+    shutil.rmtree('tmp', ignore_errors=True)
